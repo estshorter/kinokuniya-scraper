e86f82d39e7bfa6910ca4d571aed5041787fd4e4
estshorter <1430311+estshorter@users.noreply.github.com>
2020-07-16 20:17:50 +0900

    Fix test case

--- scrape_test.go
+++ scrape_test.go
@@ -29,12 +29,12 @@ func TestScrape(t *testing.T) {
 			"9783662575604",
 		},
 		{
-			"https://www.kinokuniya.co.jp/f/dsg-01-9784873117560",
-			"Effective Python―Pythonプログラムを改良する59項目",
-			"スラットキン,ブレット【著】〈Slatkin,Brett〉、黒川 利明【訳】、石本 敦夫【技術監修】",
-			3200,
+			"https://www.kinokuniya.co.jp/f/dsg-01-9784873119175",
+			"Effective Python - Pythonプログラムを改良する90項目 (第2版)",
+			"ブレット・スラットキン、黒川利明",
+			3600,
 			"オライリー・ジャパン",
-			"9784873117560",
+			"9784873119175",
 		},
 		{
 			"https://www.kinokuniya.co.jp/f/dsg-01-9784621300251",
